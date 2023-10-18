df5d894d316a873db0b1f08995ed3bcf5a1462b9
Mathius <info@yogimathius.dev>
2023-10-17 23:17:52 -0700

    fix: update belongs to in order and route

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
-  belongs_to :location
+  belongs_to :location, foreign_key: :origin_id
+  belongs_to :location, foreign_key: :destination_id
   belongs_to :client
   belongs_to :route
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,4 @@
 class Route < ApplicationRecord
-  belongs_to :location
+  belongs_to :location, foreign_key: :origin_id
+  belongs_to :location, foreign_key: :destination_id
 end
