c2657cf2663f01938a321c0144d0b19d5ef1187e
Mathius <info@yogimathius.dev>
2023-10-25 22:58:23 -0700

    chore: add comments

--- app/controllers/route_controller.rb
+++ app/controllers/route_controller.rb
@@ -28,24 +28,31 @@ class RouteController < ApplicationController
     ]
   end
   
-  # How to store array of profitable routes?
+
   def get(order: {})
     if is_valid_order?(order)
       pickup_coords = {
         latitude: order["pick_up"]["latitude"],
         longitude: order["pick_up"]["longitude"]
       }
-
+      # binding.break
       dropoff_coords = {
         latitude: order["drop_off"]["latitude"],
         longitude: order["drop_off"]["longitude"]
       }
 
+      # Find matching routes for proximity (within 1 km)
       matching_routes = Route.select do |route|
-        # binding.break
+        binding.break
         route.in_range?(pickup_coords, route) &&
           route.in_range?(dropoff_coords, route)
       end
+
+
+      # Check truck package capacity (make sure order doesn’t overload truck)
+      # Check truck shift duration (route doesn’t exceed 10 hrs)
+      # How to store array of profitable routes?
+
       if matching_routes.present?
         return [matching_routes]
       end
