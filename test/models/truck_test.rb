e04b762f232c083fb5d03621f58ae6a66dec4d3c
Mathius <info@yogimathius.dev>
2023-10-28 11:50:38 -0700

    chore: add basic test in truck_test

--- test/models/truck_test.rb
+++ test/models/truck_test.rb
@@ -3,7 +3,26 @@
 require "test_helper"
 
 class TruckTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @mock_order = {
+      cargo: {
+        packages: [1, 60, 'standard']
+      },
+      pick_up: {
+        latitude: 33.754413815792205,
+        longitude: -84.3875298776525
+      },
+      drop_off: {
+        latitude: 34.87433824316913,
+        longitude: -85.08447506395166
+      }
+    }
+
+    @truck = Truck.all.first
+  end
+
+  test "has_capacity? returns true when cargo can fit in truck" do
+    result = @truck.has_capacity?(@mock_order[:cargo])
+    assert_equal true, result
+  end
 end
