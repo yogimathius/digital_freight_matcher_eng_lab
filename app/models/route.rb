1595c31d6972991d45b0418663e1cf6ddb6574b2
Mathius <info@yogimathius.dev>
2023-10-28 11:39:44 -0700

    fix: update route matching algo
    Co-authored-by: pisco24 <pisco24@users.noreply.github.com>

--- app/models/route.rb
+++ app/models/route.rb
@@ -8,7 +8,7 @@ class Route < ApplicationRecord
   def spherical_distance(start_coords, end_coords)
     radius = 6372.8 # rough radius of the Earth, in kilometers
     lat1, long1 = deg2rad(start_coords[:latitude], start_coords[:longitude])
-    lat2, long2 = deg2rad(end_coords[:latitude], start_coords[:longitude])
+    lat2, long2 = deg2rad(end_coords[:latitude], end_coords[:longitude])
 
     2 * radius * asin(sqrt((sin((lat2 - lat1) / 2)**2) + (cos(lat1) * cos(lat2) * (sin((long2 - long1) / 2)**2))))
   end
@@ -23,15 +23,11 @@ class Route < ApplicationRecord
     distance_from_destination,
     route_distance
   )
-  binding.break
-    root1 = Math.sqrt(distance_from_destination + route_distance + distance_from_origin)
-    root2 = Math.sqrt(-distance_from_destination + route_distance + distance_from_origin)
-    root3 = Math.sqrt(distance_from_destination - route_distance + distance_from_origin)
-    root4 = Math.sqrt(distance_from_destination + route_distance - distance_from_origin)
-    # α = acos(((route_distance ** 2) + (distance_from_destination ** 2 )- (distance_from_origin ** 2)) / (2 * route_distance * distance_from_destination))
-    area = (0.25 * root1 * root2 * root3 * root4)
-
-    2 * area / route_distance
+    s = (distance_from_origin + distance_from_destination + route_distance) / 2.0
+    area = Math.sqrt(s * (s - distance_from_origin) * (s - distance_from_destination) * (s - route_distance))
+  
+    # Height of the triangle
+    triangular_height = 2 * area / route_distance
   end
 
   def in_range?(order_coords, route)
@@ -49,15 +45,18 @@ class Route < ApplicationRecord
       route.origin,
       route.destination
     )
+    # binding.break
+    if distance_from_origin < 1 || distance_from_destination < 1
+      # binding.break
+      return true
+    end
 
-    return true unless distance_from_origin > 1 && distance_from_destination > 1
-
-
-    triangular_height = get_triangular_height(
-      distance_from_origin,
-      distance_from_destination,
-      route_distance
-    )
+    # if distance_from_origin < 1 || distance_from_destination < 1
+      # binding.break
+      # return true
+    # end
+    # b
+    triangular_height = get_triangular_height(distance_from_origin,     distance_from_destination,route_distance)
     puts triangular_height
     triangular_height < 1
   end
