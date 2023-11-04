c3b6701d968a116744441201fdd4cc3d5568fbad
Mathius <info@yogimathius.dev>
2023-11-04 10:57:42 -0700

    feat: expose matching_routes in api

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -67,6 +67,57 @@ class RoutesController < ApplicationController
     end
   end
 
+  def get_matching_routes(order: {})
+    return nil unless valid_order?(order)
+
+    pickup_coords = {
+      latitude: order[:pick_up][:latitude],
+      longitude: order[:pick_up][:longitude]
+    }
+
+    dropoff_coords = {
+      latitude: order[:drop_off][:latitude],
+      longitude: order[:drop_off][:longitude]
+    }
+
+    # Find matching routes for proximity (within 1 km)
+    matching_pick_up_routes = get_routes_in_range(pickup_coords)
+
+    matching_drop_off_routes = get_routes_in_range(dropoff_coords)
+
+    # Check truck package capacity (make sure order doesn’t overload truck)
+    # matching_routes = matching_routes.filter do |route|
+
+    # end
+    # Check truck shift duration (route doesn’t exceed 10 hrs)
+    # matching_routes = matching_routes.filter do |route|
+
+    # end
+
+    routes_found = matching_pick_up_routes.present? && matching_drop_off_routes.present?
+
+    # What should we really return? I think a message would be nice for the merchant at this point
+    return nil unless routes_found
+
+    matching_pick_up_routes.filter do |route|
+      matching_drop_off_routes.map(&:id).include?(route.id)
+    end
+  end
+
+  def valid_order?(order)
+    return false if order.empty?
+
+    return false if order[:pick_up].nil?
+
+    true
+  end
+
+  def get_routes_in_range(order_coords)
+    Route.select do |route|
+      route.in_range?(order_coords, route)
+    end
+  end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    get '/matching_routes', to: 'routes_controller#get_matching_routes'
+  end
 end
