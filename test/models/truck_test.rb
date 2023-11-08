ebc9dbd9427ebce39e3e9b45f6e7f1793762ca6b
pisco24 <boydakar101@gmail.com>
2023-11-07 19:06:24 -0500

    Truck capacity

--- test/models/truck_test.rb
+++ test/models/truck_test.rb
@@ -22,7 +22,23 @@ class TruckTest < ActiveSupport::TestCase
   end
 
   test "has_capacity? returns true when cargo can fit in truck" do
+    cargo = {
+      max_weight: 9180,       
+      total_volume: 1700,     
+      pallet_per_truck: 26.6, 
+  }
     result = @truck.has_capacity?(@mock_order[:cargo])
     assert_equal true, result
+  end
+
+  test "has_capacity? returns false when cargo exceeds truck's capacity" do
+    cargo = {
+      max_weight: 10000,     
+      total_volume: 2000,   
+      pallet_per_truck: 30, 
+    }
+
+    result = @truck.has_capacity?(@mock_order[:cargo])
+    assert_equal false, result
   end
 end
