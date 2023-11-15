7c0d31385f8e4390b9af6a589471db9ac997dd3f
Mathius <info@yogimathius.dev>
2023-11-14 17:11:52 -0800

    feat: add single order distance profitability calculator

--- app/models/route.rb
+++ app/models/route.rb
@@ -8,28 +8,37 @@ class Route < ApplicationRecord
   has_one :truck, dependent: :destroy
   has_many :orders, dependent: :destroy
 
-  def in_range?(order_coords, route)
+  def get_distances(order_coords)
     distance_from_origin = spherical_distance(
       order_coords,
-      route.origin
+      origin
     )
 
     distance_from_destination = spherical_distance(
       order_coords,
-      route.destination
+      destination
     )
 
-    route_distance = spherical_distance(
-      route.origin,
-      route.destination
-    )
-    # binding.break
-    if distance_from_origin < 1 || distance_from_destination < 1
-      # binding.break
-      return true
-    end
+    [distance_from_origin, distance_from_destination]
+  end
 
-    return true unless distance_from_origin > 1 && distance_from_destination > 1
+  def order_coords(order)
+    pickup_coords = {
+      latitude: order[:pick_up][:latitude],
+      longitude: order[:pick_up][:longitude]
+    }
+
+    dropoff_coords = {
+      latitude: order[:drop_off][:latitude],
+      longitude: order[:drop_off][:longitude]
+    }
+    [pickup_coords, dropoff_coords]
+  end
+
+  def in_range?(order_coords)
+    distance_from_origin, distance_from_destination = get_distances(order_coords)
+
+    return true if distance_from_origin < 1 || distance_from_destination < 1
 
     triangular_height = get_triangular_height(
       distance_from_origin,
@@ -38,5 +47,42 @@ class Route < ApplicationRecord
     )
     Rails.logger.debug triangular_height
     triangular_height < 1
+  end
+
+  def profitability(order)
+    pickup_coords, dropoff_coords = order_coords(order)
+
+    pickup_origin_distance, pickup_destination_distance = get_distances(pickup_coords)
+
+    dropoff_origin_distance, dropoff_destination_distance = get_distances(dropoff_coords)
+
+    pickup_from_origin = pickup_origin_distance < dropoff_origin_distance
+
+    hypotenuse = pickup_from_origin ? dropoff_origin_distance : dropoff_destination_distance
+
+    hypotenuse *= hypotenuse
+
+    triangular_height = get_triangular_height(
+      pickup_origin_distance,
+      pickup_destination_distance,
+      route_distance
+    )
+
+    triangular_height *= triangular_height
+
+    distance_from_pickup_to_dropoff = sqrt(hypotenuse + triangular_height)
+
+    total_distance = distance_from_pickup_to_dropoff + triangular_height
+
+    # return total cost per kilometer
+    # multiply by 1.6 to get per km value
+    ((total_distance * 1.6) * 0.02) * 1.50
+  end
+
+  def route_distance
+    spherical_distance(
+      origin,
+      destination
+    )
   end
 end
--- test/models/route_test.rb
+++ test/models/route_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class RouteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @mock_order = {
+      cargo: {
+        packages: [1, 60, 'standard']
+      },
+      pick_up: {
+        latitude: 33.754413815792205,
+        longitude: -84.3875298776525
+      },
+      drop_off: {
+        latitude: 34.87433824316913,
+        longitude: -85.08447506395166
+      }
+    }
+
+    @mock_route = routes(:route1)
+  end
+
+  test "profitability" do
+    profitability = @mock_route.profitability(@mock_order)
+
+    # hacky test assertion but at least will catch changes in the codebase that affect this function
+    assert_equal("6.75", format('%.2f', profitability))
+  end
 end
