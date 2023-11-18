d1ea99d530da371d86294a8416a32c052c725034
Mathius <info@yogimathius.dev>
2023-11-17 22:29:10 -0800

    feat: add build_order method

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @mock_order = {
+      cargo: {
+        packages: [1, 60, 'standard']
+      },
+      pick_up: {
+        latitude: 33.754413815792205,
+        longitude: -84.3875298776525
+      },
+      drop_off: {
+        latitude: 34.87433824316913,
+        longitude: -85.08447506395166
+      }
+    }
+    @mock_route = routes(:route1)
+  end
+
+  test "build_order should create a client, package, locations, cargo and order with params" do
+    client = Client.create!
+    result = Order.new.build_order(@mock_order, @mock_route, client)
+
+    expected = Order.last
+    assert_equal expected, result
+  end
 end
