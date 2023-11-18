618811b27748ae8c33c53baad846e226f624aa9f
Mathius <info@yogimathius.dev>
2023-11-17 23:12:46 -0800

    refactor: use helpers in route model

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,40 +1,12 @@
 class Route < ApplicationRecord
   include Math
+  include CoordinateHelper
+  include GeometryHelper
 
   belongs_to :origin, class_name: 'Location'
   belongs_to :destination, class_name: 'Location'
   has_one :truck, dependent: :destroy
   has_many :orders, dependent: :destroy
-
-  # rubocop:disable Metrics/AbcSize
-  def spherical_distance(start_coords, end_coords)
-    radius = 6372.8 # rough radius of the Earth, in kilometers
-    lat1, long1 = deg2rad(start_coords[:latitude], start_coords[:longitude])
-    lat2, long2 = deg2rad(end_coords[:latitude], end_coords[:longitude])
-
-    2 * radius * asin(sqrt((sin((lat2 - lat1) / 2)**2) + (cos(lat1) * cos(lat2) * (sin((long2 - long1) / 2)**2))))
-  end
-  # rubocop:enable Metrics/AbcSize
-
-  def deg2rad(lat, long)
-    [(lat * Math::PI / 180), (long * Math::PI / 180)]
-  end
-
-  # Using Heron's formula:
-  def get_triangular_height(
-    distance_from_origin,
-    distance_from_destination,
-    route_distance
-  )
-    root1 = Math.sqrt(distance_from_destination + route_distance + distance_from_origin)
-    root2 = Math.sqrt(-distance_from_destination + route_distance + distance_from_origin)
-    root3 = Math.sqrt(distance_from_destination - route_distance + distance_from_origin)
-    root4 = Math.sqrt(distance_from_destination + route_distance - distance_from_origin)
-    # α = acos(((route_distance ** 2) + (distance_from_destination ** 2 )- (distance_from_origin ** 2)) / (2 * route_distance * distance_from_destination))
-    area = (0.25 * root1 * root2 * root3 * root4)
-
-    2 * area / route_distance
-  end
 
   def in_range?(order_coords, route)
     distance_from_origin = spherical_distance(
