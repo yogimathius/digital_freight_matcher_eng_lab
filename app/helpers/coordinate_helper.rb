f6326a6c8550f2a22833624ec62f56d33f57f217
Mathius <info@yogimathius.dev>
2023-11-24 23:32:01 -0800

    chore: update `in_range?` to accept proximity arg

--- app/helpers/coordinate_helper.rb
+++ app/helpers/coordinate_helper.rb
@@ -10,8 +10,8 @@ module CoordinateHelper
   # rubocop:disable Metrics/AbcSize
   def spherical_distance(start_coords, end_coords)
     radius = 6372.8 # rough radius of the Earth, in kilometers
-    lat1, long1 = deg2rad(start_coords[:latitude], start_coords[:longitude])
-    lat2, long2 = deg2rad(end_coords[:latitude], end_coords[:longitude])
+    lat1, long1 = deg2rad(start_coords[:latitude].to_f, start_coords[:longitude].to_f)
+    lat2, long2 = deg2rad(end_coords[:latitude].to_f, end_coords[:longitude].to_f)
 
     2 * radius * asin(sqrt((sin((lat2 - lat1) / 2)**2) + (cos(lat1) * cos(lat2) * (sin((long2 - long1) / 2)**2))))
   end
