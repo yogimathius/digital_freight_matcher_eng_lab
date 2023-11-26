59186b68eb6bff4d5e153f905e8d794dab533d74
Mathius <info@yogimathius.dev>
2023-11-25 23:23:46 -0800

    feat: add rspec tests for location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,9 @@
+# app/models/location.rb
+
 class Location < ApplicationRecord
   has_many :routes, dependent: :destroy
   has_many :orders, dependent: :destroy
+
+  validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 end
