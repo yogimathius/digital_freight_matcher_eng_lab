e131b37e1acb16b62998032996df7e249467a008
Mathius <info@yogimathius.dev>
2023-11-25 21:18:37 -0800

    fix: assert posting mock order matches ringold route

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -3,6 +3,20 @@ require "test_helper"
 class OrdersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @order = orders(:order1)
+
+    @mock_order = {
+      cargo: {
+        packages: [1, 60, 'standard']
+      },
+      pick_up: {
+        latitude: 33.754413815792205,
+        longitude: -84.3875298776525
+      },
+      drop_off: {
+        latitude: 34.87433824316913,
+        longitude: -85.08447506395166
+      }
+    }
   end
 
   test "should get index" do
@@ -15,13 +29,13 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should create order" do
-  #   assert_difference("Order.count") do
-  #     post orders_url, params: { order: { client_id: @order.client_id, destination_id: @order.destination_id, origin_id: @order.origin_id, route_id: @order.route_id } }
-  #   end
+  test "should create order matching ringold route" do
+    result = post orders_url, params: { order: @mock_order }
 
-  #   assert_redirected_to order_url(Order.last)
-  # end
+    ringold_route = Route.find(1)
+
+    assert ringold_route, result
+  end
 
   test "should show order" do
     get order_url(@order)
