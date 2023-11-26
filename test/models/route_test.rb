1ea185b9a78a11e3b07d3d576cbbd011e4e83b49
Mathius <info@yogimathius.dev>
2023-11-25 21:03:18 -0800

    refactor: use order fixture instead of mock json data in route_test

--- test/models/route_test.rb
+++ test/models/route_test.rb
@@ -4,19 +4,7 @@ class RouteTest < ActiveSupport::TestCase
   include OrdersHelper
 
   setup do
-    @mock_order = {
-      cargo: {
-        packages: [1, 60, 'standard']
-      },
-      pick_up: {
-        latitude: 33.754413815792205,
-        longitude: -84.3875298776525
-      },
-      drop_off: {
-        latitude: 34.87433824316913,
-        longitude: -85.08447506395166
-      }
-    }
+    @mock_order = orders(:order1)
 
     @mock_route = routes(:route1)
   end
