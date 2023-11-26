d972d891338eb7d299513e0f1d36ce43ff6cf61f
Mathius <info@yogimathius.dev>
2023-11-25 21:09:10 -0800

    chore: move csv's to proper dir

--- test/helpers/orders_helper_test.rb
+++ test/helpers/orders_helper_test.rb
@@ -10,7 +10,7 @@ class OrdersHelperTest < ActiveSupport::TestCase
   end
 
   test "in_range? bulk validates pickup and dropoff points from one km test data" do
-    one_km_data = CSV.open("test/1_km_radius_pair.csv", headers: :first_row).map(&:to_h)
+    one_km_data = CSV.open("test/fixtures/files/1_km_radius_pair.csv", headers: :first_row).map(&:to_h)
 
     one_km_data.each do |location|
       location_hash = location.transform_keys(&:to_sym)
@@ -23,7 +23,7 @@ class OrdersHelperTest < ActiveSupport::TestCase
   end
 
   test "in_range? bulk validates pickup and dropoff points from ten km test data" do
-    ten_km_data = CSV.open("test/10_km_radius_pair.csv", headers: :first_row).map(&:to_h)
+    ten_km_data = CSV.open("test/fixtures/files/10_km_radius_pair.csv", headers: :first_row).map(&:to_h)
 
     ten_km_data.each do |location|
       location_hash = location.transform_keys(&:to_sym)
