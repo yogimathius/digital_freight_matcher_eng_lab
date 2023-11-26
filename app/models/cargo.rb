4f7575adb16d8906b6c3f2b7ffcda9b8573fca8d
Mathius <info@yogimathius.dev>
2023-11-25 23:13:11 -0800

    feat: add rspec tests for cargo model

--- app/models/cargo.rb
+++ app/models/cargo.rb
@@ -1,7 +1,12 @@
+# app/models/cargo.rb
+
 class Cargo < ApplicationRecord
   belongs_to :order
   belongs_to :truck
   has_many :packages, dependent: :destroy
 
   accepts_nested_attributes_for :packages, allow_destroy: true
+
+  validates :order_id, presence: true
+  validates :truck_id, presence: true
 end
