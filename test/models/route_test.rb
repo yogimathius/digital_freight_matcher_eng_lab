fac9ee29c315fd0a39801d15c161220da0d03736
Mathius <info@yogimathius.dev>
2023-11-28 23:47:12 -0800

    fix: use ENV var to run large test suites

--- test/models/route_test.rb
+++ test/models/route_test.rb
@@ -18,44 +18,50 @@ class RouteTest < ActiveSupport::TestCase
     assert_equal(@mock_route.price_based_on_cargo_cost + profitability(@mock_route.orders.first, @mock_route), route_profit)
   end
 
-  one_km = CSV.open("test/fixtures/files/1_km_radius_pair.csv", headers: :first_row).map(&:to_h)
-  order_args = one_km.each_slice(2).to_a
-  order_args.each do |pickup, dropoff|
-    pickup_hash = pickup.transform_keys(&:to_sym)
-    dropoff_hash = dropoff.transform_keys(&:to_sym)
-
-    expected = pickup_hash[:valid]
-    order_params = {
-      pick_up: pickup_hash,
-      drop_off: dropoff_hash
-    }
-
-    test "in_range? bulk validates pickup and dropoff points from 1 km test data #{pickup_hash[:id]}" do
-      is_valid = Route.routes_in_range(order_params, 1).any?
-
-      assert_equal expected, is_valid.to_s
-    end
+  def self.run_large_test_suite?
+    ENV['RUN_LARGE_TEST_SUITE'].to_i == 1
   end
 
-  ten_km = CSV.open("test/fixtures/files/10_km_radius_pair.csv", headers: :first_row).map(&:to_h)
-  order_args = ten_km.each_slice(2).to_a
-  order_args.each do |pickup, dropoff|
-    pickup_hash = pickup.transform_keys(&:to_sym)
-    dropoff_hash = dropoff.transform_keys(&:to_sym)
+  if run_large_test_suite?
+    one_km = CSV.open("test/fixtures/files/1_km_radius_pair.csv", headers: :first_row).map(&:to_h)
+    order_args = one_km.each_slice(2).to_a
+    order_args.each do |pickup, dropoff|
+      pickup_hash = pickup.transform_keys(&:to_sym)
+      dropoff_hash = dropoff.transform_keys(&:to_sym)
 
-    expected = pickup_hash[:valid]
-    order_params = {
-      pick_up: pickup_hash,
-      drop_off: dropoff_hash
-    }
+      expected = pickup_hash[:valid]
+      order_params = {
+        pick_up: pickup_hash,
+        drop_off: dropoff_hash
+      }
 
-    test "in_range? bulk validates pickup and dropoff points from ten km test data #{pickup_hash[:id]}" do
-      routes = Route.routes_in_range(order_params, 1)
+      test "in_range? bulk validates pickup and dropoff points from 1 km test data #{pickup_hash[:id]}" do
+        is_valid = Route.routes_in_range(order_params, 1).any?
 
-      is_valid = routes.any?
-      # binding.break if is_valid.to_s != expected
+        assert_equal expected, is_valid.to_s
+      end
+    end
 
-      assert_equal expected, is_valid.to_s
+    ten_km = CSV.open("test/fixtures/files/10_km_radius_pair.csv", headers: :first_row).map(&:to_h)
+    order_args = ten_km.each_slice(2).to_a
+    order_args.each do |pickup, dropoff|
+      pickup_hash = pickup.transform_keys(&:to_sym)
+      dropoff_hash = dropoff.transform_keys(&:to_sym)
+
+      expected = pickup_hash[:valid]
+      order_params = {
+        pick_up: pickup_hash,
+        drop_off: dropoff_hash
+      }
+
+      test "in_range? bulk validates pickup and dropoff points from ten km test data #{pickup_hash[:id]}" do
+        routes = Route.routes_in_range(order_params, 1)
+
+        is_valid = routes.any?
+        # binding.break if is_valid.to_s != expected
+
+        assert_equal expected, is_valid.to_s
+      end
     end
   end
 end
