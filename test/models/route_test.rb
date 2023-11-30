31dbd43e133f5e44b74f9cb10ba36a5447e3ad46
Mathius <info@yogimathius.dev>
2023-11-29 22:34:00 -0800

    chore: add tests for find_matching_routes_for_order

--- test/models/route_test.rb
+++ test/models/route_test.rb
@@ -18,6 +18,34 @@ class RouteTest < ActiveSupport::TestCase
     assert_equal(@mock_route.price_based_on_cargo_cost + profitability(@mock_route.orders.first, @mock_route), route_profit)
   end
 
+  test "find_matching_routes_for_order returns routes if order fits in shift" do
+    matching_routes = Route.find_matching_routes_for_order(@mock_order)
+
+    assert matching_routes.any?
+
+    route1 = matching_routes.first
+
+    9.times do
+      create_mock_order
+    end
+
+    route1.save!
+
+    matching_routes = Route.find_matching_routes_for_order(@mock_order)
+
+    assert matching_routes.any?
+  end
+
+  test "find_matching_routes_for_order returns empty array if order doesn't fit in shift" do
+    10.times do
+      create_mock_order
+    end
+
+    matching_routes = Route.find_matching_routes_for_order(@mock_order)
+
+    assert matching_routes.empty?
+  end
+
   def self.run_large_test_suite?
     ENV['RUN_LARGE_TEST_SUITE'].to_i == 1
   end
@@ -63,5 +91,33 @@ class RouteTest < ActiveSupport::TestCase
         assert_equal expected, is_valid.to_s
       end
     end
+  end
+
+  def create_mock_order
+    truck1 = trucks(:truck1)
+    ringgold = locations(:ringgold)
+    atlanta = locations(:atlanta)
+    client1 = clients(:client1)
+
+    order1 = Order.create!(
+      origin_id: ringgold.id,
+      destination_id: atlanta.id,
+      client: client1,
+      route: @mock_route
+    )
+
+    cargo1 = Cargo.create!(
+      order: order1,
+      truck: truck1
+    )
+
+    Package.create!(
+      volume: 10,
+      weight: 50,
+      package_type: "Package Type X",
+      cargo: cargo1
+    )
+
+    order1
   end
 end
