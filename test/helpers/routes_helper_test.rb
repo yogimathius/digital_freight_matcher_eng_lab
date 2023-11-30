5d6a888af2356c8c502ae91d582b8ba48c5ee88f
Mathius <info@yogimathius.dev>
2023-11-29 22:38:57 -0800

    fix: update assertions in routes helper test, add create order method

--- test/helpers/routes_helper_test.rb
+++ test/helpers/routes_helper_test.rb
@@ -10,23 +10,51 @@ class RoutesHelperTest < ActiveSupport::TestCase
 
   test "fits_in_shift? returns true when order fits within 10 hour shift duration of route1" do
     result = fits_in_shift?(@order1, @route1)
-    assert result, true
+    assert_equal result, true
   end
 
   test "fits_in_shift? returns false when order does not fit within 10 hour shift duration of route1" do
     9.times do
-      @route1.orders << @order1
+      create_mock_order
     end
 
     @still_fits = orders(:order1)
     # Should return true when adding 9 orders
     result = fits_in_shift?(@still_fits, @route1)
-    assert result, true
+    assert_equal result, true
     # add one more
     @route1.orders << @still_fits
 
     @doesnt_fit = orders(:order1)
     result = fits_in_shift?(@doesnt_fit, @route1)
-    assert result, false
+    assert_equal result, false
+  end
+
+  def create_mock_order
+    truck1 = trucks(:truck1)
+    ringgold = locations(:ringgold)
+    atlanta = locations(:atlanta)
+    client1 = clients(:client1)
+
+    order1 = Order.create!(
+      origin_id: ringgold.id,
+      destination_id: atlanta.id,
+      client: client1,
+      route: @route1
+    )
+
+    cargo1 = Cargo.create!(
+      order: order1,
+      truck: truck1
+    )
+
+    Package.create!(
+      volume: 10,
+      weight: 50,
+      package_type: "Package Type X",
+      cargo: cargo1
+    )
+
+    order1
   end
 end
