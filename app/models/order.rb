545abe795ffde2d4d94a33a5263b573ae27f8b5e
Mathius <info@yogimathius.dev>
2023-12-02 11:02:35 -0800

    chore: add backlog reference to order, make route optional

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,9 @@ class Order < ApplicationRecord
   belongs_to :origin, class_name: 'Location'
   belongs_to :destination, class_name: 'Location'
   belongs_to :client
-  belongs_to :route
+  belongs_to :route, optional: true
+  belongs_to :backlog, optional: true, foreign_key: { to_table: 'backlog' }, inverse_of: :orders
+
   has_one :cargo, dependent: :destroy
 
   accepts_nested_attributes_for :origin, :destination, :cargo, allow_destroy: true
